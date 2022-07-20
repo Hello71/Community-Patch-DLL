899ab9e7a061ade39bb992773e068fdf236a4d8d
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2022-07-20 19:59:44 -0400

    FFastAllocator: throw if full, don't return UINT_MAX (?!?)

--- FirePlace/include/FireWorks/FFastAllocator.h
+++ FirePlace/include/FireWorks/FFastAllocator.h
@@ -619,7 +619,11 @@ void* operator new(size_t uiSize, FFixedBlockAllocator< T, bPODType, AllocPool, 
 	if( kAlloc.m_uiSize == kAlloc.m_uiCapacity )
 	{
 		FAssertMsg(false, "Fixed-size allocator is full!" );
-		return UINT_MAX;
+#if defined(__cpp_exceptions) || defined(_CPPUNWIND) || defined(__EXCEPTIONS)
+		throw std::bad_alloc();
+#else
+		abort();
+#endif
 	}
 
 	unsigned int uiPos;
